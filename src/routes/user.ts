de384f80d1d65e3a151ecd096ce35f6ef39ccce2
shivkumarojha <ojhashivkumar88@gmail.com>
2024-06-05 17:55:19 +0530

    user route error handling done with try catch block

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -15,17 +15,28 @@ userRouter.post('/signup', async (c) => {
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
 
-    const user = await c.req.json()
+    try {
+        const user = await c.req.json()
 
-    await prisma.user.create({
-        data: {
-            email: user.email,
-            name: user.name,
-            password: user.password
-        }
-    })
-    console.log("hello", user)
-    return c.text("From signup")
+        await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password: user.password
+            }
+        })
+        console.log("hello", user)
+        return c.json({
+            message: "User Created Successfully",
+            user: user
+        })
+    } catch (error) {
+        c.status(403)
+        c.json({
+            message: "Some error occured while creating user",
+            error: error
+        })
+    }
 })
 
 // POST / api / v1 / user / signin
@@ -37,30 +48,38 @@ userRouter.post('/signin', async (c) => {
         datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate())
 
-    const isUser = await prisma.user.findUnique({
-        where: {
-            email: email
-        },
-        select: {
-            id: true,
-            password: true
+    try {
+
+        const isUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            },
+            select: {
+                id: true,
+                password: true
+            }
+        })
+        if (!isUser) {
+            c.json({
+                message: "email doesn't exist, Kindly sign up"
+            })
         }
-    })
-    if (!isUser) {
+        if (password !== isUser?.password) {
+            c.json({
+                message: "Password didn't matched"
+            })
+        }
+        const token = await sign({ id: isUser?.id, email: email }, c.env.JWT_SECRET)
+        c.status(200)
+        return c.json({
+            message: "Successfully logged in",
+            token: token
+        })
+    } catch (error) {
         c.json({
-            message: "email doesn't exist, Kindly sign up"
+            message: "Something bad happend",
+            error: error
         })
     }
-    if (password !== isUser?.password) {
-        c.json({
-            message: "Password didn't matched"
-        })
-    }
-    const token = await sign({ id: isUser?.id, email: email }, c.env.JWT_SECRET)
-    c.status(200)
-    return c.json({
-        message: "Successfully logged in",
-        token: token
-    })
 
 })
